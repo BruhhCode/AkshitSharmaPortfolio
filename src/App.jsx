93030017636b2d7b8dca25afd94987bc5173f244
BruhhCode <akshits055@gmail.com>
2026-04-02 15:50:38 +0530

    changes in the bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import Silk from './Silk';
 
+<Silk
+  speed={5}
+  scale={1}
+  color="#7B7481"
+  noiseIntensity={1.5}
+  rotation={0}
+/>
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'motion/react'
 import Header from './components/Header'
